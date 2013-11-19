7e1f7b98548146d73eaae8b807dfd84dfd3f321e
phsh <phhedman@gmail.com>
2013-11-19 08:23:11 +0100

    added the alpha handling of colors.

--- script/partical/particle.js
+++ script/partical/particle.js
@@ -22,7 +22,8 @@ function Particle(init_x,init_y, xSpeed, ySpeed){
 	this.y = init_y;
 	this.xSpeed = xSpeed;
 	this.ySpeed = ySpeed;
-	this.fillColor = 'rgb( 0 , 0, 255 )';
+	this.fillColor = 'rgba( 0 , 0, 255, 1.0 )';
+	this.alpha = 1.0;
 }
 
 Particle.prototype.update = function(){
@@ -35,13 +36,14 @@ Particle.prototype.update = function(){
 	}
 	if (this.x > canvas.width){
 		this.xSpeed = -this.xSpeed;
-	//	this.startColor = this.startColor - 10;
+		this.alpha = this.alpha - 0.1;
 	}
 	if (this.x < 0){
 		this.xSpeed = -this.xSpeed;
-	//	this.startColor = this.startColor - 10;
+		this.alpha = this.alpha - 0.1;
 	}
-	//if(this.startColor < 1) this.startColor = 127;
+	if(this.alpha < 0) this.alpha = 1.0;
+	this.fillColor='rgba( 0 , 0, 255, '+this.alpha+' )';
 }
 
 Particle.prototype.drawCircle = function(context){
@@ -54,26 +56,15 @@ Particle.prototype.drawCircle = function(context){
 	context.stroke();
 	
 }
-
 var particles  = {};
-
 for(i=0; i<250; i++){
 	var p = new Particle(random(490,510), 500, random(-5,5), random(-15,-5));
 	particles[i] = p;
-	if((i % 3)===0){
-		particles[i].fillColor = 'rgb( 0 , 255, 0 )';
-	}
-	if((i % 3)===1){
-		particles[i].fillColor = 'rgb( 255 , 0, 0 )';
-	}
-	if((i % 3)===2){
-		particles[i].fillColor = 'rgb( 0 , 0, 255 )';
-	}
+	
 }
 
 myTimer();
-var startColor = 255;
-
+var startColor = 1.0;
 
 function myTimer(){
 	window.requestAnimationFrame(myTimer);
@@ -85,21 +76,10 @@ function myTimer(){
 		count += particles[i].y;
 	}
 	if(count > 245*500){
+		//startColor = startColor - 0.1;
 		for(i=0; i<250; i++){
 			particles[i].ySpeed = random(-15,-5);
-			if((i % 3)===0){
-				particles[i].fillColor = 'rgb( 0 , ' + startColor + ', 0 )';
-			}
-			if((i % 3)===1){
-				particles[i].fillColor = 'rgb( ' + startColor + ' , 0, 0 )';
-			}
-			if((i % 3)===2){
-				particles[i].fillColor = 'rgb( 0 , 0, ' + startColor + ' )';
-			}
-			startColor = startColor - 5;
-			if(startColor < 5) startColor = 255;
+			//if(startColor < 0) startColor = 1.0;
 		}
 	}
 }
-
-//var myVar=setInterval(function(){myTimer()},200);
