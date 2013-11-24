17f5fa89969ecc745e61f9707486828d0c3445a6
phsh <phhedman@gmail.com>
2013-11-24 16:20:30 +0100

    removed dead code

--- script/partical/particle.js
+++ script/partical/particle.js
@@ -1,5 +1,6 @@
 var random = require('./random').random;
 var randomInt = require('./random').randomInt;
+var get_random_color = require('./random').get_random_color;
 var gameAreaHeight=600;
 var gameAreaWidth= 1200;
 var gameAreaZ = 100;
@@ -7,20 +8,23 @@ var ballSize = 35;
 var bounceIndex=0.75;
 var gravity = 0.20;
 var canvas = document.createElement('canvas');
-
-
+var bounce = false;
+var enable_bounce = true;
+var frames = 0;
+var color  = get_random_color();
+var xSpeed = random(-15,15);
+var ySpeed = getNewYSpeed();
+var particleCount = 100;
+var particles  = new Array();
 canvas.height=gameAreaHeight;
 canvas.width=gameAreaWidth;
 document.body.appendChild(canvas);
-
-var particleCount = 100;
 
 var context = canvas.getContext('2d');
 
 function Particle(init_x,init_y, xSpeed, ySpeed,color){
 	this.x = init_x;
 	this.y = init_y;
-	this.z = 50;
 	this.xSpeed = xSpeed;
 	this.ySpeed = ySpeed;
 	this.zSpeed = random(-1,1);
@@ -31,18 +35,12 @@ function Particle(init_x,init_y, xSpeed, ySpeed,color){
 Particle.prototype.update = function(){
 	this.x = this.x + this.xSpeed;
 	this.y = this.y + this.ySpeed;
-	this.z = this.z + this.zSpeed;
 	this.ySpeed = this.ySpeed + gravity;
-	var zIndex = (gameAreaZ -this.z);
 	if (this.y > (gameAreaHeight-this.radie ) ) {
 		this.ySpeed = -this.ySpeed;
 		this.ySpeed = bounceIndex * this.ySpeed;
 		this.y = gameAreaHeight-this.radie;
 	}
-	
-	if(this.z < 0) this.zSpeed = Math.abs(this.zSpeed);
-	if(this.z > gameAreaZ) this.zSpeed = - Math.abs(this.zSpeed);
-	
 	if (this.y < this.radie) {
 		if(this.ySpeed < 0) {
 			this.ySpeed = -this.ySpeed;
@@ -61,31 +59,13 @@ Particle.prototype.update = function(){
 
 Particle.prototype.drawCircle = function(context){
 	context.beginPath();
-	var zIndex = ((gameAreaZ-this.z));
-	//var size = (this.radie)*(zIndex/100) + 1;
 	var size = this.radie;
 	context.arc(this.x, this.y, size, 0, 2 * Math.PI, false);
 	context.fillStyle = this.fillColor;
 	context.fill();
-	context.lineWidth =3;// (3)*((gameAreaZ-this.z)/100) + 1;
+	context.lineWidth =3;
 	context.strokeStyle = '#000000';
 	context.stroke();
-	
-}
-
-function drawReferenseLine(context){
-	context.fillStyle="#000000";
-	context.fillRect(101,gameAreaHeight-101,gameAreaWidth-202,1);
-	for(var i = 101; i < gameAreaHeight-101; i++){
-		context.fillRect(101,i,1,1);
-		context.fillRect(gameAreaWidth-101,i,1,1);
-	}
-	context.fillRect(101,gameAreaHeight-101,gameAreaWidth-202,1);
-	context.fillRect(101,101,gameAreaWidth-202,1);
-	context.fillRect(51,gameAreaHeight-51,gameAreaWidth-102,1);
-	context.fillRect(51,51,gameAreaWidth-102,1);
-	context.fillRect(1,gameAreaHeight-1,gameAreaWidth-2,1);
-	context.fillRect(1,1,gameAreaWidth-2,1);
 	
 }
 
@@ -97,35 +77,17 @@ function debugLine(count,debugLineValue,context){
 	context.fillRect(0,debugLineValue,gameAreaWidth,1);
 }
 
-var particles  = new Array();
 
-function get_random_color() {
-    var letters = '0123456789ABCDEF'.split('');
-    var color = '#';
-    for (var i = 0; i < 6; i++ ) {
-        color += letters[Math.round(Math.random() * 15)];
-    }
-    return color;
-}
-
-var bounce = false;
-var enable_bounce = true;
-var frames = 0;
-var color  = get_random_color();
-var xSpeed = random(-15,15);
-var ySpeed = getNewYSpeed();
-myTimer();
 function getNewYSpeed(){
 	return random(-45,-25);
 }
+
 function myTimer(){
 	window.requestAnimationFrame(myTimer);
 	context.clearRect(0,0,gameAreaWidth,gameAreaHeight);
 	var count = 0;
 	frames++;
-	
-	
-	
+
 	for(i=0; i<particles.length; i++){
 		count += particles[i].y;
 		particles[i].drawCircle(context);
@@ -133,24 +95,13 @@ function myTimer(){
 		
 	}
 	var debugLineValue = (frames>10)?(gameAreaHeight-40):gameAreaHeight;
-	/*
-	if(frames > 600) {
-		var xRestart=getNewYSpeed();
-		var yRestart=random(-15,15);
-		for(i=0; i<particles.length; i++){
-			particles[i].ySpeed = xRestart;
-			particles[i].xSpeed = yRestart;
-		}
-		frames=0;
-	}
-	*/
 	if(count > particles.length*debugLineValue){
 		if(bounce){
 		var xRestart=getNewYSpeed();
 		var yRestart=random(-15,15);	
 		for(i=0; i<particles.length; i++){
 			particles[i].ySpeed = -45;
-			//particles[i].xSpeed = yRestart;
+
 		}
 		bounce=false;
 		frames=0;
@@ -173,7 +124,7 @@ function myTimer(){
 		}
 	}
 	
-	
 	debugLine(count,debugLineValue,context);
-//	drawReferenseLine(context);
 }
+
+myTimer();
--- script/partical/random.js
+++ script/partical/random.js
@@ -5,6 +5,16 @@ function random(min, max) {
 function randomInt(min, max){
 	return Math.floor( Math.random() * (max + min + 1) ) + min;
 }
+function get_random_color() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.round(Math.random() * 15)];
+    }
+    return color;
+}
+
 
 module.exports.random = random;
 module.exports.randomInt = randomInt;
+module.exports.get_random_color = get_random_color;
