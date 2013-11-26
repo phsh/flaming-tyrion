2eeaa3395d14011f651d23cbec4bd837e09c71c8
phsh <phhedman@gmail.com>
2013-11-26 09:35:46 +0100

    removed unused function

--- particle/particle_updater.js
+++ particle/particle_updater.js
@@ -25,27 +25,7 @@ module.exports.ParticleUpdater = ParticleUpdater;
 function getNewYSpeed(){
 	return random(-55,-25);
 }
+
 function getNewXSpeed(){
 	return random(-30,30);
-}
-
-function stateUpperLeftCorner(){
-	var xSpeedNew = -12;
-	var ySpeedNew =  -45;
-	var xStartNew = 100;
-	var yStartNew = 100;
-	for(i=0; i<particles.length; i++){
-		var index_i = i % 5;
-		if(index_i === 0) {
-			xSpeedNew = getNewXSpeed();
-			ySpeedNew = getNewYSpeed();
-		}
-		
-		particles[i].ySpeed = xSpeedNew+index_i;
-		particles[i].xSpeed = ySpeedNew+index_i;
-		particles[i].y = ballSize;
-		particles[i].x = ballSize;
-	}
-	timeDelayCounter.reset();
-	stateMachine.setState(7);
 }
