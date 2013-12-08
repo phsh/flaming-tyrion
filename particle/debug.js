2f1f972c37fe58ddfd893ba5d9008e096dfca9cf
phsh <phhedman@gmail.com>
2013-12-08 21:52:25 +0100

    renamed variable

--- particle/debug.js
+++ particle/debug.js
@@ -1,12 +1,10 @@
 function Debug(){
-
 }
-
 
 Debug.prototype.displayDebugLine = function (baselineCount,debugLineValue,context,world){
 		context.fillStyle="#FF0000";
-		var whereAreTheBalls = (baselineCount/world.particleCount);
-		context.fillRect(0,whereAreTheBalls,world.areaWidth,1);
+		var averageBallLine = (baselineCount/world.particleCount);
+		context.fillRect(0,averageBallLine,world.areaWidth,1);
 		context.fillStyle="#00FF00";
 		context.fillRect(0,debugLineValue,world.areaWidth,1);
 }
