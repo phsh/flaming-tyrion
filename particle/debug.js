067607256b9632383b7f05f4ae0c64b43780e934
phsh <phhedman@gmail.com>
2013-12-09 10:36:26 +0100

    changed structure for clarity

--- particle/debug.js
+++ particle/debug.js
@@ -2,9 +2,11 @@ function Debug(){
 }
 
 Debug.prototype.displayDebugLine = function (baselineCount,debugLineValue,context,world){
-	context.fillStyle="#FF0000";
 	var averageBallLine = (baselineCount/world.particleCount);
+
+	context.fillStyle="#FF0000";
 	context.fillRect(0,averageBallLine,world.areaWidth,1);
+	
 	context.fillStyle="#00FF00";
 	context.fillRect(0,debugLineValue,world.areaWidth,1);
 }
