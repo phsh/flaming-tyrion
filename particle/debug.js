e6a215e0aeb07ef28b4d6e5571093e146123f32a
phsh <phhedman@gmail.com>
2013-12-09 10:41:19 +0100

    [CleanCode] renamed a variable

--- particle/debug.js
+++ particle/debug.js
@@ -1,14 +1,14 @@
 function Debug(){
 }
 
-Debug.prototype.displayDebugLine = function (baselineCount,debugLineValue,context,world){
+Debug.prototype.displayDebugLine = function (baselineCount,particlesInRestValue,context,world){
 	var averageBallLine = (baselineCount/world.particleCount);
-
+	
 	context.fillStyle="#FF0000";
 	context.fillRect(0,averageBallLine,world.areaWidth,1);
 	
 	context.fillStyle="#00FF00";
-	context.fillRect(0,debugLineValue,world.areaWidth,1);
+	context.fillRect(0,particlesInRestValue,world.areaWidth,1);
 }
 
 module.exports.Debug = Debug;
